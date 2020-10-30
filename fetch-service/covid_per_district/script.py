04aa64fdaa912947a128e2c5a98aabe67ee0effd
wkrl <kwxrl@yandex.com>
2020-10-30 16:17:30 +0100

    Read db credentials from .env

--- fetch-service/covid_per_district/script.py
+++ fetch-service/covid_per_district/script.py
@@ -1,9 +1,16 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 import requests
 import pycouchdb
 import urllib3
 import json
 
 urllib3.disable_warnings()
+
+# load .env 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # get data
 res = requests.get("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=death_rate,cases,deaths,last_update,recovered,GEN,cases_per_100k,cases_per_population&outSR=4326&f=geojson")
@@ -26,7 +33,8 @@ obj = {
     "data": data
 }
 
-server = pycouchdb.Server("http://admin:password@couchdb.internal.datexis.com/")
+server_url = "http://{}:{}@couchdb.internal.datexis.com/".format(os.environ.get("COUCHDB_USERNAME"), os.environ.get("COUCHDB_PASSWORD"))
+server = pycouchdb.Server(server_url)
 db = server.database("covid_per_district")
 
 if not any(document["doc"]["date"] == obj["date"] for document in db.all()):
