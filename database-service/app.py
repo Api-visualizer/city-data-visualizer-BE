b1144c6bae02d5155ed4f118a2cc4181cde4066b
Pierre Fabarius <pierre.fabarius@gmail.com>
2020-11-19 20:34:16 +0100

    [add] route:/berlin-accidents which expects year parameter

--- database-service/app.py
+++ database-service/app.py
@@ -1,11 +1,11 @@
-import os
+import os, sys
 
 import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property  # fix for "ImportError: cannot import name 'cached_property'"
 
 from cloudant import Cloudant
 from flask import Flask
-from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource, reqparse
 
 import atexit
 
@@ -26,7 +26,7 @@ URL = os.environ.get('DB_URL')
 client = Cloudant(USER, PASS, url=URL, connect=True, auto_renew=True)
 
 
-# classes
+# routes
 @api.route('/api/v1/berlin-verschenken')
 class BerlinVerschenken(Resource):
     def get(self):
@@ -51,6 +51,46 @@ class BerlinCovidDistrict(Resource):
         return get_table_data_latest('berlin_covid_district'), 200
 
 
+@api.route('/api/v1/berlin-accidents')
+class BerlinCovidDistrict(Resource):
+    def get(self):
+        # Define parser and request args
+        parser = reqparse.RequestParser()
+        parser.add_argument('year', type=int, required=True, help='set a year')
+        args = parser.parse_args()
+        year = str(args['year'])
+        return get_table_data('berlin_accidents', year), 200
+
+
+# fetch all entries from table
+def get_table_data(table_name, param=None):
+    try:
+        table_data = client[table_name]
+        if param:
+            payload = table_data[param]
+        else:
+            payload = [data for data in table_data]
+        if payload:
+            return payload, 200
+    except Exception as e:
+        print('ERROR: Could not fetch table {}. Cause: {}'.format(table_name, e))
+        return 'No data available. Try changing the value of the parameter', 400
+
+
+
+# fetch latest entry from table
+def get_table_data_latest(table_name):
+    try:
+        table_data = client[table_name]   # not sure how only retreive the last document.
+        payload = [data for data in table_data]
+        if payload:
+            return payload[0], 200 # this method is not optimal
+        return 'No data available. Please validate your request and try again.', 400
+    except Exception as e:
+        print('ERROR: Could not fetch table >{}<. Cause: {}'.format(table_name, e))
+        return e, 404
+
+
 # disconnct from db on server shutdown
 @atexit.register
 def shutdown():
@@ -59,28 +99,6 @@ def shutdown():
         print('disconnected from db')
     except Exception:
         pass
-
-
-# fetch all entries from table
-def get_table_data(table_name):
-    try:
-        table_data = client[table_name]
-        payload = [data for data in table_data]
-        return payload
-    except Exception as e:
-        print('ERROR: Could not fetch table {}. Cause: {}'.format(table_name, e))
-        return 'Something went wrong.', 404
-
-
-# fetch latest entry from table
-def get_table_data_latest(table_name):
-    try:
-        table_data = client[table_name]   # not sure how only retreive the last document.
-        payload = [data for data in table_data]
-        return payload[0] # this method is not optimal
-    except Exception as e:
-        print('ERROR: Could not fetch table {}. Cause: {}'.format(table_name, e))
-        return 'Something went wrong.', 404
 
 
 if __name__ == '__main__':
