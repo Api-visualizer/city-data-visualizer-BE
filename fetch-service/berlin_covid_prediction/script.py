42aa82ade5cffac3113470227d009a2cc5f03396
wkrl <kwxrl@yandex.com>
2021-01-15 17:13:46 +0100

    Use deepcopy to copy dates array

--- fetch-service/berlin_covid_prediction/script.py
+++ fetch-service/berlin_covid_prediction/script.py
@@ -1,8 +1,8 @@
-
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
+import copy
 import urllib3
 import pycouchdb
 import numpy as np
@@ -25,8 +25,8 @@ def init_dataframe(dates, cases):
     df["dates"] = pd.to_datetime(df["dates"], format="%d.%m.%Y")
     return df.sort_values(by="dates")
 
-def find_polynomial_regression(deg):
-    coef = np.polyfit(np.arange(len(dates)), cases, deg)
+def find_polynomial_regression(deg, data):
+    coef = np.polyfit(np.arange(len(data)), data, deg)
     p = np.poly1d(coef)
     return p
 
@@ -36,19 +36,20 @@ def get_next_weeks_dates():
     last_date = datetime.utcfromtimestamp(dates_tail.values[0].astype(int)*ns)
     return [(last_date+timedelta(days=x)).strftime("%d.%m.%Y") for x in range(1, 8)]
 
-def fill_data_object(dates, cases, p):
+def fill_data_object(dates, cases, p):    
     data = {"regression": {"x": [], "y": []}, "cases": {"x": dates, "y": cases}}
 
     next_week = get_next_weeks_dates()
     data["date"] = next_week[0]
-
-    pred_dates = dates    
+    
+    pred_dates = copy.deepcopy(dates)
     pred_dates += next_week
     pred_cases = [round(p(x),2) for x in np.arange(len(dates))]
-    pred_cases += [round(p(x),2) for x in np.arange(len(pred_dates))[-7:]]
-
+    pred_cases += [round(p(x),2) for x in np.arange(len(pred_dates))[-7:]]    
+    
     data["regression"]["x"] = pred_dates
     data["regression"]["y"] = pred_cases    
+        
     return data
 
 def post_data(db, data):
@@ -60,12 +61,13 @@ if __name__ == "__main__":
 
     dates = [document["doc"]["date"] for document in db.all()] 
     cases = [document["doc"]["cases"] for document in db.all()]
-    df = init_dataframe(dates, cases)
+    df = init_dataframe(dates, cases)    
 
     deg = 9
-    p = find_polynomial_regression(deg) # TODO: Find degree that fits the best or store multiple regressions
- 
-    dates = df["dates"].dt.strftime("%d.%m.%Y").astype(str).to_list() # Convert dates
-    data = fill_data_object(dates, cases, p)
-
+    p = find_polynomial_regression(deg, df["cases"].to_list()) 
+    
+    dates = df["dates"].dt.strftime("%d.%m.%Y").astype(str).to_list() # Convert date format 
+    
+    data = fill_data_object(dates, df["cases"].to_list(), p)
     post_data(pred_db, data)
+    
